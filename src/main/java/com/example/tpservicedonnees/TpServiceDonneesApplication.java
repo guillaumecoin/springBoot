15dba81fce6ae9d7852bd1116c08410f9e7394c7
guillaumecoin <guillaume.coin@epsi.fr>
2023-07-21 14:59:30 +0200

    initial

---
+++ src/main/java/com/example/tpservicedonnees/TpServiceDonneesApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpservicedonnees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpServiceDonneesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpServiceDonneesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tpservicedonnees/TpServiceDonneesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpservicedonnees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpServiceDonneesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
